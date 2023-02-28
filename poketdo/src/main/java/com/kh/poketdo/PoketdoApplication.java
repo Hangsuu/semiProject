47dd33164de6080ac7dd962b380da16939c86262
Hangsuu <ohhangsu@naver.com>
2023-02-28 18:21:05 +0900

    poketdo

---
+++ poketdo/src/main/java/com/kh/poketdo/PoketdoApplication.java
@@ -0,0 +1,13 @@
+package com.kh.poketdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketdoApplication.class, args);
+	}
+
+}
---
+++ poketdo/src/test/java/com/kh/poketdo/PoketdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.poketdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

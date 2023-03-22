03465e14b635df546368c0e0c1cf07282c21b5e3
Dooshine <doyungi0304@gmail.com>
2023-03-22 19:24:36 +0900

    homeSample

--- poketdo/src/main/java/com/kh/poketdo/controller/HomeController.java
+++ poketdo/src/main/java/com/kh/poketdo/controller/HomeController.java
@@ -11,15 +11,8 @@ public class HomeController {
     return "/WEB-INF/views/home.jsp";
   }
 
-<<<<<<< HEAD
-    @GetMapping("/sample")
-    public String sample() {
-        return "/WEB-INF/views/sample.jsp";
-    }
-=======
   @GetMapping("/sample")
   public String sample() {
     return "/WEB-INF/views/sample.jsp";
   }
->>>>>>> c93375140659598808c897941c73855818835336
 }

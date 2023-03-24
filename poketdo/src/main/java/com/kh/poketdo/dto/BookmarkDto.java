b018ce3f748e714d3529940fe4116e6179f131ba
hangsuu <ohhangsu@naver.com>
2023-03-24 18:15:16 +0900

    북마크 기능 추가

---
+++ poketdo/src/main/java/com/kh/poketdo/dto/BookmarkDto.java
@@ -0,0 +1,13 @@
+package com.kh.poketdo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BookmarkDto {
+	private int allboardNo;
+	private String memberId;
+	private String bookmarkType;
+}
---
+++ poketdo/src/main/resources/static/js/bookmark.js
@@ -0,0 +1,55 @@
+$(function(){
+	checkBookmark();
+	function checkBookmark(){
+		$(".fa-star").each(function(){
+			var allboardNo = $(this).data("allboard-no");
+			var starBox = $(this);
+			$.ajax({
+				url:"/rest/bookmark/check",
+				method:"post",
+				data:{
+					allboardNo:allboardNo,
+					memberId:memberId,
+				},
+				success:function(response){
+					if(response==true){
+						starBox.removeClass("fa-regular").addClass("fa-solid");
+					}
+					else{
+						starBox.removeClass("fa-solid").addClass("fa-regular");
+					}
+				},
+				error:function(){
+					alert("통신에러");
+				}
+			});					
+		});		
+	}
+	
+	$(".fa-star").click(function(){ 
+		var allboardNo = $(this).data("allboard-no");
+		var starBox = $(this);
+		var bookmarkType = $(this).data("bookmark-type");
+		$.ajax({
+			url:"/rest/bookmark/",
+			method:"post",
+			data:{
+				allboardNo:allboardNo,
+				memberId:memberId,
+				bookmarkType:bookmarkType,
+			},
+			success:function(response){
+				if(response==true){
+					starBox.removeClass("fa-solid fa-regular").addClass("fa-solid");
+				}
+				else{
+					starBox.removeClass("fa-solid fa-regular").addClass("fa-regular");
+				}
+			},
+			error:function(){
+				alert("통신에러");
+			}
+		});
+	});
+	
+});

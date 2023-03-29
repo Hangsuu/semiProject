02da3d217612abe204358fd821ad964162c2a4dd
ishouldtryit@naver.com <ishouldtryit@naver.com>
2023-03-29 09:57:41 +0900

    수정

--- poketdo/src/main/java/com/kh/poketdo/dto/PocketmonJoinTypeDto.java
+++ poketdo/src/main/java/com/kh/poketdo/dto/PocketmonJoinTypeDto.java
@@ -13,6 +13,7 @@ public class PocketmonJoinTypeDto {
 
   //포켓몬스터 기본 정보 + 타입 연결 테이블 DTO
 
-  private int pocketJoinNo;
-  private int typeJoinNo; // snake_case에서 camelCase로 바꿈
+	private int joinNo;
+	private int pocketJoinNo;
+	private int typeJoinNo; // snake_case에서 camelCase로 바꿈
 }
